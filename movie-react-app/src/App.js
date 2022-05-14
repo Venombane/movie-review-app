95f57595509380ad62d2757a0934760e2b82ba2a
Venombane <63618043+Venombane@users.noreply.github.com>
2022-05-14 20:08:06 -0230

    Movies Displayed
    Reads the json file and displays the contents to the page.

--- movie-react-app/src/App.js
+++ movie-react-app/src/App.js
@@ -1,11 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import { MovieList } from './Movies';
 
 function App() {
+  // let movies = {};
+  const [movies, setMovies] = useState(null);
+
+  useEffect( () => {
+    fetch("./movies.json")
+    .then( response => response.json() )
+    .then( setMovies )
+    .then( console.log(movies))
+    .catch( e => console.log(e.message) );
+  }, [])
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <MovieList movies={movies} setMovies={setMovies}/>
+    </>
   );
 }
 
