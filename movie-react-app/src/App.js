8282e02d2dd3ce33c26d2b3853caa78a49b37352
Venombane <starfighter_ndga@hotmail.com>
2022-05-16 22:01:54 -0230

    Changing computers
    Moving to my other computer

--- movie-react-app/src/App.js
+++ movie-react-app/src/App.js
@@ -2,9 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { useState, useEffect } from 'react';
 import { MovieList } from './Movies';
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
+import FormPage from './FormPage';
+
 
 function App() {
-  // let movies = {};
   const [movies, setMovies] = useState(null);
 
   useEffect( () => {
@@ -14,11 +19,16 @@ function App() {
     .then( console.log(movies))
     .catch( e => console.log(e.message) );
   }, [])
+
+  
   return (
-    <>
+    <Router>
       <MovieList movies={movies} setMovies={setMovies}/>
-    </>
+      <Route path='/FormPage' component={FormPage} />
+    </Router>
   );
+    
+  
 }
 
 export default App;
---
+++ movie-react-app/src/FormPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const FormPage = () => (
+    <>
+    <h1>New Movie Form</h1>
+
+    </>
+);
+
+export default FormPage;
